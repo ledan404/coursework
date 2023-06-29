b4947dca07665b19bf214a1e88d7c8bf9d1415a9
ledan404 <alfa7.plast@gmail.com>
2023-06-29 19:18:14 +0300

    add start file

---
+++ src/main/java/ua/lviv/iot/Film/FilmApplication.java
@@ -0,0 +1,13 @@
+package ua.lviv.iot.Film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
+public class FilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmApplication.class, args);
+	}
+}
